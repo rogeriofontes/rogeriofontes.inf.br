ff225ed9d71a17afeb41fd46ee2958d98395f6b9
Rogerio Fontes <fontestz@gmail.com>
2025-10-25 12:24:12 -0300

    Ajuste no build

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
-const repo = 'rogeriofontes.inf.br'
-// Se for publicar em https://SEU-USUARIO.github.io/REPO,
-// troque "/REPO" abaixo pelo nome do seu repositório.
-// Se for publicar em https://SEU-USUARIO.github.io (repo especial),
-// deixe basePath como ''.
-const basePath = isProd ? '/REPO' : ''
 
-const nextConfig = {
-  experimental: { typedRoutes: true },
+export default {
   output: 'export',
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : undefined,
-  trailingSlash: true, // ajuda com GitHub Pages
-  images: { unoptimized: true }
+  basePath: isProd ? '/rogeriofontes.inf.br' : '',  // << só basePath
+  trailingSlash: true,
+  images: { unoptimized: true } // apenas se você usar <Image />
 }
-export default nextConfig;
